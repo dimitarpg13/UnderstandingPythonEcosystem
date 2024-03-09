31a527da7124223b20a38ad8ee0e5293a836d56f
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-03-09 12:08:46 -0500

    add script example

---
+++ illustration_code/low_level_api/concurrent.futures/deadlock_with_Future.py
@@ -0,0 +1,18 @@
+import concurrent.futures
+import time
+
+def wait_on_b():
+    time.sleep(5)
+    print(b.result()) # b ill never complete because it is waiting on a.
+    return 5
+
+def wait_on_a():
+    time.sleep(5)
+    print(a.result()) # a will never complete because it is waiting on b.
+    return 6
+
+executor = ThreadPoolExecutor(max_workers=2)
+a = executor.submit(wait_on_b)
+b = executor.submit(wait_on_a)
+
+print(f"Done!"")
