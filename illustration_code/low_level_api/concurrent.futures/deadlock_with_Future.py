f5c5ce736d58543b4a26d333695540d513af5923
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-03-09 12:34:16 -0500

    Update deadlock_with_Future.py

--- illustration_code/low_level_api/concurrent.futures/deadlock_with_Future.py
+++ illustration_code/low_level_api/concurrent.futures/deadlock_with_Future.py
@@ -19,3 +19,17 @@ b = executor.submit(wait_on_a)
 
 # this line of code will be executed but the program will never exit!
 print(f"Done!")
+
+# after hitting Ctrl-C we are going to get the following exception due to the fact that lock was not acquired
+#
+# Exception ignored in: <module 'threading' from '/Users/dimitargueorguiev/.pyenv/versions/3.12.1/lib/python3.12/threading.py'>
+# Traceback (most recent call last):
+#  File ".../.pyenv/versions/3.12.1/lib/python3.12/threading.py", line 1593, in _shutdown
+#    atexit_call()
+#  File ".../.pyenv/versions/3.12.1/lib/python3.12/concurrent/futures/thread.py", line 31, in _python_exit
+#    t.join()
+#  File ".../.pyenv/versions/3.12.1/lib/python3.12/threading.py", line 1147, in join
+#    self._wait_for_tstate_lock()
+#  File ".../.pyenv/versions/3.12.1/lib/python3.12/threading.py", line 1167, in _wait_for_tstate_lock
+#    if lock.acquire(block, timeout):
+#       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
