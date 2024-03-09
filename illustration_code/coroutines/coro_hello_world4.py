c3bd4b4bddfb85907931e25ea06dc6e24ba40506
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-03-09 08:55:04 -0500

    Update coro_hello_world4.py

--- illustration_code/coroutines/coro_hello_world4.py
+++ illustration_code/coroutines/coro_hello_world4.py
@@ -5,7 +5,7 @@ async def say_after(delay, what):
     await asyncio.sleep(delay)
     print(what)
 
-# current execution of coroutines using TaskGroup 
+# concurrent execution of coroutines using TaskGroup 
 async def main():
     async with asyncio.TaskGroup() as tg:
         task1 = tg.create_task(
