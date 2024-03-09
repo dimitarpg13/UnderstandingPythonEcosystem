4f74ac443756a52891f6dac6a8a926edd5afd94a
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-03-09 12:29:21 -0500

    Update deadlock_with_Future.py

--- illustration_code/low_level_api/concurrent.futures/deadlock_with_Future.py
+++ illustration_code/low_level_api/concurrent.futures/deadlock_with_Future.py
@@ -17,5 +17,5 @@ executor = ThreadPoolExecutor(max_workers=2)
 a = executor.submit(wait_on_b)
 b = executor.submit(wait_on_a)
 
-# this line of code will never be reached!
+# this line of code will be executed!
 print(f"Done!"")
