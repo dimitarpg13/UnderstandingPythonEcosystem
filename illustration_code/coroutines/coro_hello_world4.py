9b88f7ef4105be58180efbbf9c99eb32b6d51bfb
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-03-09 08:54:47 -0500

    Update coro_hello_world4.py

--- illustration_code/coroutines/coro_hello_world4.py
+++ illustration_code/coroutines/coro_hello_world4.py
@@ -1,1 +1,23 @@
+import asyncio
+import time
 
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+# current execution of coroutines using TaskGroup 
+async def main():
+    async with asyncio.TaskGroup() as tg:
+        task1 = tg.create_task(
+            say_after(1, 'hello'))
+          
+        task2 = tg.create_task(
+            say_after(2, 'world'))
+
+        print(f"started at {time.strftime('%X')}")
+
+# the await is implicit when the context manager exits
+print(f"finished at {time.strftime('%X')}")
+        
+
+asyncio.run(main())
